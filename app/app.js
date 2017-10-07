12014fc9d02fcb782eb53347d642ad301b438dc5
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-10-07 14:17:57 -0500

    login partial created

--- app/app.js
+++ app/app.js
@@ -6,8 +6,14 @@ const app = angular.module('tnaComm', ['ngRoute']);
 app.config(($routeProvider) => {
 	$routeProvider
 	.when('/', {
-		templateUrl: 'partials/landing.html',
+		templateUrl: 'partials/landing.html'
+	})
+	.when('/login', {
+		templateUrl: 'partials/login.html',
 		controller: 'userCtrl'
+	})
+	.when('/home', {
+		templateUrl: 'partials/home.html'
 	})
 	.when('/upload', {
 		templateUrl: 'partials/upload-view.html',
